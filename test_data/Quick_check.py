19338a26016840f09a794d796a1041eba855350e
Jackn104 <85194816+Jackn104@users.noreply.github.com>
2022-04-29 01:35:51 -0500

    Add files via upload

---
+++ test_data/Quick_check.py
@@ -0,0 +1,24 @@
+import sys
+#Finds all the data in the file an writes it to a csv
+with open('uuid_scan.txt') as ff:
+    lines = ff.readlines()
+
+all = ''
+info = ''
+prnt = False
+for line in lines:
+    if 'time' in line:
+        info += line[6:-1] + ', '
+    if 'RSSI' in line:
+        info += line[line.find('=') +2:-1] + ', '
+    if "UUIDs = dbus.Array" in line:
+        if "UUIDs = dbus.Array([dbus.String(" in line:
+            info += line[37:73]
+            all += info + '\n'
+            info = ''
+        else:
+            info = ''
+    
+
+with open('uuid_parsed.csv', 'w') as aa:
+    aa.write(all)
